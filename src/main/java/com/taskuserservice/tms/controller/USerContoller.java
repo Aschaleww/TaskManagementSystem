bc99986018faaad2226e4b2ecf9060217f44da2e
Aschalew <aschalewbekele2127@gmail.com>
2025-10-22 22:38:18 -0400

    Add some thing

--- src/main/java/com/taskuserservice/tms/controller/USerContoller.java
+++ src/main/java/com/taskuserservice/tms/controller/USerContoller.java
@@ -1,2 +1,14 @@
-package com.taskuserservice.tms.controller;public class USerContoller {
+package com.taskuserservice.tms.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping
+public class USerContoller {
+
+    public ResponseEntity<String> createtask(){
+        return new ResponseEntity("firstclas", HttpStatus.CREATED);
+    }
+
 }
